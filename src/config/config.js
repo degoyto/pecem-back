6af939de5327d3ec30989c0b585aa60d944745f4
degoyto <48868413+degoyto@users.noreply.github.com>
2019-12-20 10:36:14 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
     // teste
     
-    port: process.env.PORT || 8081,
+    port: process.env.PORT,
     db:{
-        database: process.env.DB_NAME || "teste",
-        user: process.env.DB_USER || "root",
-        password: process.env.DB_PASS || "root",
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
         options:{ 
-            dialect: process.env.DIALECT || "mysql",
-            host: process.env.HOST || "localhost",
+            dialect: process.env.DIALECT,
+            host: process.env.HOST,
             storage:".tabtracker.sql"
         }
     },
